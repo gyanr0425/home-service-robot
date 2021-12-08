3304ce4c2b697bce8f2395956749938426dd8be5
Hyomuk <gyanr0425@gmail.com>
2021-12-08 04:13:29 +0000

    remove absolute path using rospack from scripts

--- src/scripts/test_navigation.sh
+++ src/scripts/test_navigation.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch" &
-xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/map/muks.world" &
+xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find pick_objects)/map/muks.world" &
 sleep 5
 # xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch" &
-xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=/home/workspace/catkin_ws/src/map/map.yaml" &
+xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(rospack find pick_objects)/map/map.yaml" &
 sleep 5
 xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch"
--- src/scripts/test_slam.sh
+++ src/scripts/test_slam.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch" &
-xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/map/muks.world" &
+xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find pick_objects)/map/muks.world" &
 sleep 5
 # xterm -e "roslaunch turtlebot_gazebo gmapping_demo.launch" &
-xterm -e "roslaunch turtlebot_gazebo gmapping_demo.launch map_file:=/home/workspace/catkin_ws/src/map/map.yaml" &
+xterm -e "roslaunch turtlebot_gazebo gmapping_demo.launch map_file:=$(rospack find pick_objects)/map/map.yaml" &
 sleep 5
 xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5
